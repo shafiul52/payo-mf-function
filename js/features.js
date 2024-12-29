3cd4b5fa8e12e379a7068f4bf158f96073f07574
Your Name <shafiulhossain52@gmail.com>
2024-12-30 01:42:54 +0600

    transactions history addedc

--- js/features.js
+++ js/features.js
@@ -1,6 +1,20 @@
 document.getElementById('show-add-money-form')
-.addEventListener('click',function(event){
-   console.log('show add money button clicked');
-   showSectionByid('add-money-form');
+.addEventListener('click', function(){
 
+console.log('show add money button clicker')
+   showSectionByid('add-money-form');
 })
+
+
+document.getElementById('show-cash-out-form').addEventListener('click',function(){
+
+
+    showSectionByid('cash-out-form');
+});
+
+
+document.getElementById('show-transaction-history').addEventListener('click',function(){
+  
+    showSectionByid('transaction-section');
+})
+
--- js/utilities2.js
+++ js/utilities2.js
@@ -14,5 +14,12 @@ function getTextFieldValueByid(id){
 
 
 function showSectionByid(id){
-    
+  // hide all the sections
+  document.getElementById('add-money-form').classList.add('hidden');
+  document.getElementById('cash-out-form').classList.add('hidden');
+  document.getElementById('transaction-section').classList.add('hidden');
+
+  // show the section with the provide id as parameter
+  document.getElementById(id).classList.remove('hidden');
 }
+  
